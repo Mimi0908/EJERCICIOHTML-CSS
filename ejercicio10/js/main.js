04862e88bce766653138ecc676e8a9452586e5c6
Mimi0908 <emilygarciamontes@gmail.com>
2023-11-28 16:57:10 -0500

    modificaciones

--- ejercicio10/js/main.js
+++ ejercicio10/js/main.js
@@ -2,9 +2,11 @@
 function agregarImg() {
     const boton = document.getElementById("boton");
     for(let i=1; i<=20; i++){
-        const imagen = `mascota${i}.jpg`;
+        const imagen = `./img/mascota${i}.jpg`;
+        const precio= document.getElementById("abajo");
         const contenedor = document.getElementById(i.toString());
         contenedor.insertAdjacentHTML("beforeend", `<img src=${imagen} alt=${imagen} class="imgBody">`);
+        contenedor.insertAdjacentHTML("beforeend", `<p class="p">$5.000</p> <h3 class="des">Veterinaria</h3>`);
     }
     boton.disabled=true;
 }
